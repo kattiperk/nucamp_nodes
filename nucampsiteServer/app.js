042024743b01572839a801b635eb1b0ea2495c43
Kateryna Masiuk <kmasiuk91@gmail.com>
2025-03-06 12:19:58 -0800

    Express Session Part 2

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -160,4 +160,25 @@ function auth(req, res, next) {
     }
 }
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+function auth(req, res, next) {
+    console.log(req.session);
+
+    if (!req.session.user) {
+        const err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
+    } else {
+        if (req.session.user === 'authenticated') {
+            return next();
+        } else {
+            const err = new Error('You are not authenticated!');
+            err.status = 401;
+            return next(err);
+        }
+    }
+}
+
 module.exports = app;
