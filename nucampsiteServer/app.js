701db0bec923b8b096d48f158859b71c39a0038a
Kateryna Masiuk <kmasiuk91@gmail.com>
2025-03-06 12:17:54 -0800

    Express Session Part 1

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -8,6 +8,9 @@ const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {});
 
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
 connect.then(() => console.log('Connected correctly to server'),
   err => console.log(err)
 );
@@ -79,7 +82,50 @@ function auth(req, res, next) {
 
 app.use(auth);
 
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+function auth(req, res, next) {
+  console.log(req.session);
+
+  if (!req.session.user) {
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+          const err = new Error('You are not authenticated!');
+          res.setHeader('WWW-Authenticate', 'Basic');
+          err.status = 401;
+          return next(err);
+      }
+
+      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+      const user = auth[0];
+      const pass = auth[1];
+      if (user === 'admin' && pass === 'password') {
+          req.session.user = 'admin';
+          return next(); // authorized
+      } else {
+          const err = new Error('You are not authenticated!');
+          res.setHeader('WWW-Authenticate', 'Basic');
+          err.status = 401;
+          return next(err);
+      }
+  } else {
+      if (req.session.user === 'admin') {
+          return next();
+      } else {
+          const err = new Error('You are not authenticated!');
+          err.status = 401;
+          return next(err);
+      }
+  }
+}
 
 function auth(req, res, next) {
     if (!req.signedCookies.user) {
