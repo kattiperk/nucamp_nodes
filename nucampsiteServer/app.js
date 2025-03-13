7ac6e0561444d392d94c1e3f4d0f99e1ef96baad
Kateryna Masiuk <kmasiuk91@gmail.com>
2025-03-13 10:23:46 -0700

    File Upload

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -30,6 +30,9 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 
+const uploadRouter = require('./routes/uploadRouter');
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -49,6 +52,9 @@ app.use('/partners', partnerRouter);
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use('/imageUpload', uploadRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
